c681e173846e81263ae35c9466eea5d1cdd92e2c
Bruno <85034205+brunoaguiar1986@users.noreply.github.com>
2026-04-08 19:59:13 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.static('public'))
+
+var porta = 3000
+
+app.listen(3000, () => console.log("Servidor rodando no link - " + `http://localhost:${porta}`))
